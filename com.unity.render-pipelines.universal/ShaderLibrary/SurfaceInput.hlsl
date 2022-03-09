eaeeb23042bb18fd42a2afdd9a0718302560f043
Matt Blair <blair1618@gmail.com>
2022-03-08 20:54:46 -0500

    Enable alpha cut-out using AlphaToMask in Lit shader

--- com.unity.render-pipelines.universal/ShaderLibrary/SurfaceInput.hlsl
+++ com.unity.render-pipelines.universal/ShaderLibrary/SurfaceInput.hlsl
@@ -21,9 +21,11 @@ half Alpha(half albedoAlpha, half4 color, half cutoff)
     half alpha = color.a;
 #endif
 
-// #if defined(_ALPHATEST_ON)
-//     clip(alpha - cutoff);
-// #endif
+#if defined(_ALPHATEST_ON)
+    // (ASG) Sharpen alpha boundary using technique from:
+    // https://bgolus.medium.com/anti-aliased-alpha-test-the-esoteric-alpha-to-coverage-8b177335ae4f
+    alpha = (alpha - cutoff) / max(fwidth(alpha), 0.0001) + 0.5;
+#endif
 
     return alpha;
 }
